8af06dfb7d88ba0f7f37d5ae13d46b871cf861e4
soham Kulkarni <soham18262@gmail.com>
2023-07-18 11:21:36 +0530

    changes in heading

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         {/* styles with _ underscore are from our own globals.css  */}
             Discover & Share
             <br className="max-md:hidden"/>
-            <span className="text-center orange_gradient">Creative prompts</span>
+            <span className="text-center orange_gradient"> Creative prompts</span>
         </h1>
         <p className="desc text-center" > 
             Promptopia is a open-source tool for modern world to discover,create and share creative and favourite prompts
